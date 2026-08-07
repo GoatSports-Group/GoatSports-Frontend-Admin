45d6d4b098676b44486cbe586fb50139a3838f9a
MinhDat1312 <nguyenthangdat84@gmail.com>
2026-08-07 13:32:32 +0700

    feat: environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
 
-    authApiUrl: "",
-    adminApiUrl: "",
-    clientApiUrl: "",
+    authApiUrl: process.env['NG_APP_AUTH_API_URL'] || "http://localhost:4400",
+    adminApiUrl: process.env['NG_APP_ADMIN_API_URL'] || "http://localhost:4300",
+    clientApiUrl: process.env['NG_APP_CLIENT_API_URL'] || "http://localhost:4200",
 
-    apiUrl: "",
+    apiUrl: process.env['NG_APP_API_URL'] || "http://localhost:7070",
 };
